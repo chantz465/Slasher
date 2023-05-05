7e94cb99bed204d7ae4d7b48b44c8110432ccc6d
Chance Gardner <Chantz465@gmail.com>
2023-05-05 09:33:01 -0600

    added test sprites

--- Slasher/level.py
+++ Slasher/level.py
@@ -1,5 +1,8 @@
+
 import pygame
-from setting import *
+from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -14,11 +17,15 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row in WORLD_MAP:
-            print(row)
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x' :
+                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
+
 
     def run(self):
         # update and draw the game
-        pass
-
+        self.visible_sprites.draw(self.display_surface)
 
--- Slasher/tile.py
+++ Slasher/tile.py
@@ -1,8 +1,10 @@
 import pygame
 from settings import *
+from debug import debug
 
-class Tile(pygame.sprite.Sprite):
+class Tile(pygame.sprite.Sprite): 
     def __init__(self,pos,groups):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        debug('Hello')
+        self.rect = self.image.get_rect(topleft = pos) 
