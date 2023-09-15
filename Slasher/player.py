3366aec0423ccac234d0e420bb3f90741a3d0461
Chance Gardner <Chantz465@gmail.com>
2023-09-15 01:05:56 -0600

    adding the inputs for attacking

--- Slasher/player.py
+++ Slasher/player.py
@@ -8,27 +8,47 @@ class Player(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-26)
 
+
+		# movement 
 		self.direction = pygame.math.Vector2()
 		self.speed = 5
+		self.attacking = False
+		self.attack_cooldown = 400
+		self.attack_time = 0
 
 		self.obstacle_sprites = obstacle_sprites
+
+		
 
 	def input(self):
 		keys = pygame.key.get_pressed()
 
-		if keys[pygame.K_UP]:
+		# movement input
+		if keys[pygame.K_w]:
 			self.direction.y = -1
-		elif keys[pygame.K_DOWN]:
+		elif keys[pygame.K_s]:
 			self.direction.y = 1
 		else:
 			self.direction.y = 0
 
-		if keys[pygame.K_RIGHT]:
+		if keys[pygame.K_d]:
 			self.direction.x = 1
-		elif keys[pygame.K_LEFT]:
+		elif keys[pygame.K_a]:
 			self.direction.x = -1
 		else:
 			self.direction.x = 0
+
+		# attack input
+		if keys[pygame.K_SPACE] and not self.attacking:
+			self.attacking = True
+			self.attack_time = pygame.time.get_ticks()
+			print('attack')
+
+		# magic input
+		if keys[pygame.K_LCTRL] and not self.attacking:
+			self.attacking = True
+			self.attack_time = pygame.time.get_ticks()
+			print('Magic')
 
 	def move(self,speed):
 		if self.direction.magnitude() != 0:
@@ -51,6 +71,7 @@ class Player(pygame.sprite.Sprite):
 					if self.direction.x < 0: # moving left
 						self.hitbox.left = sprite.hitbox.right
 
+
 		if direction == 'vertaical':
 			for sprite in self.obstacle_sprites:
 				if sprite.hitbox.colliderect(self.hitbox):
@@ -59,7 +80,15 @@ class Player(pygame.sprite.Sprite):
 					if self.direction.y < 0: # moving up
 						self.hitbox.top = sprite.hitbox.bottom
 
+	def cooldowns(self):
+		current_time = pygame.time.get_ticks()
+
+		if self.attacking:
+			if current_time - self.attack_time >= self.attack_cooldown:
+				self.attacking = False
+
 	def update(self):
 		self.input()
+		self.cooldowns()
 		self.move(self.speed)
 
