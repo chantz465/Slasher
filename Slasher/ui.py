802ced414db5daff34456a3d2f83e58bb00ee370
Chance Gardner <Chantz465@gmail.com>
2023-09-17 03:47:57 -0600

    added energy bar and black out line for bars

--- Slasher/ui.py
+++ Slasher/ui.py
@@ -12,8 +12,23 @@ class UI:
 
         # bar setup
         self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
-        self.health_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+        self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+
+    def show_bar(self,current,max_amount,bg_rect,color):
+        # draw bg
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
+
+        # coverting stat to pixel
+        ratio = current / max_amount
+        current_width = bg_rect.width * ratio
+        current_rect = bg_rect.copy()
+        current_rect.width = current_width
+
+        # drawing the bar
+        pygame.draw.rect(self.display_surface,color,current_rect)
+        pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
 
 
     def display(self,player):
-        pygame.draw.rect(self.display_surface,'black', self.health_bar_rect)
+        self.show_bar(player.health, player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
+        self.show_bar(player.energy, player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
