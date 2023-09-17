6c2a5e7025edcf11bcb74d059dd81f0270739f50
Chance Gardner <Chantz465@gmail.com>
2023-09-17 03:20:26 -0600

    added health bar to top left

--- Slasher/level.py
+++ Slasher/level.py
@@ -6,6 +6,7 @@ from player import Player
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
 
 
 class Level:
@@ -22,6 +23,9 @@ class Level:
 
         # sprite setup
         self.create_map()
+
+        # user interface
+        self.ui = UI()
 
 
 
@@ -80,6 +84,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.ui.display(self.player)
         
 
 class YSortCameraGroup(pygame.sprite.Group):
--- Slasher/player.py
+++ Slasher/player.py
@@ -20,7 +20,6 @@ class Player(pygame.sprite.Sprite):
 
 		# movement 
 		self.direction = pygame.math.Vector2()
-		self.speed = 5
 		self.attacking = False
 		self.attack_cooldown = 400
 		self.attack_time = None
@@ -37,6 +36,13 @@ class Player(pygame.sprite.Sprite):
 		self.can_switch_weapon = True
 		self.weapon_swtich_time = None
 		self.switch_duration_cooldown = 200
+
+		# stats
+		self.stats = {'health' : 100, 'energy' : 60,'attack' : 10,'magic' : 4,'speed' : 5}
+		self.health = self.stats['health']
+		self.energy = self.stats['energy']
+		self.exp = 123
+		self.speed = self.stats['speed']
 
 
 
