43f275009377053a28735c6ac0acef3f5154560d
Chance Gardner <Chantz465@gmail.com>
2023-09-18 22:54:43 -0600

    switch up the weapon ui and added a gold flash when swtiching weapons

--- Slasher/ui.py
+++ Slasher/ui.py
@@ -14,6 +14,14 @@ class UI:
         self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
 
+
+        #convert weapon dictionary
+        self.weapon_graphics = []
+        for weapon in weapon_data.values():
+            path = weapon['graphic']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+
     def show_bar(self,current,max_amount,bg_rect,color):
         # draw bg
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
@@ -47,10 +55,23 @@ class UI:
 
         self.show_exp(player.exp)
 
-        self.selection_box(10,630) # weapon
-        self.selection_box(85,640) # magic
+        self.weapon_overlay(player.weapon_index,not player.can_switch_weapon)
+        #self.selection_box(85,640) # magic
 
-    def selection_box(self,left,top):
+    def selection_box(self,left,top,has_switched):
         bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
         pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
+        if has_switched:
+            pygame.draw.rect(self.display_surface,UI_BORDER_COLOR_ACTIVE,bg_rect,3)
+        else:
+            pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
+        return bg_rect
+
+
+    def weapon_overlay(self,weapon_index,has_switched):
+        bg_rect = self.selection_box(10,630,has_switched) # weapon
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center = bg_rect.center)
+
+        self.display_surface.blit(weapon_surf,weapon_rect)
