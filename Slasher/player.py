3dba033d91217fbc04888565b63a355892e7ef03
Chance Gardner <Chantz465@gmail.com>
2023-09-19 20:51:36 -0600

    stated the code for the enemy and refactor some of the player code into enityso the enemy can call on the movment code without calling player

--- Slasher/player.py
+++ Slasher/player.py
@@ -1,8 +1,10 @@
 import pygame 
 from settings import *
 from support import import_folder
+from entity import Entity
 
-class Player(pygame.sprite.Sprite):
+
+class Player(Entity):
 	def __init__(self,pos,groups,obstacle_sprites,create_attack,destory_attack,create_magic):
 		super().__init__(groups)
 		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
@@ -146,35 +148,9 @@ class Player(pygame.sprite.Sprite):
 			if 'attack' in self.status:
 				self.status = self.status.replace('_attack','')
 
-	def move(self,speed):
-		if self.direction.magnitude() != 0:
-			self.direction = self.direction.normalize()
-
-		self.hitbox.x += self.direction.x * speed
-		self.collision('horizontal')
-		self.hitbox.y += self.direction.y * speed
-		self.collision('vertaical')
-		self.rect.center = self.hitbox.center
-
-		
-
-	def collision(self,direction):
-		if direction == 'horizontal':
-			for sprite in self.obstacle_sprites:
-				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.x > 0: # moving right
-						self.hitbox.right = sprite.hitbox.left
-					if self.direction.x < 0: # moving left
-						self.hitbox.left = sprite.hitbox.right
+	
 
 
-		if direction == 'vertaical':
-			for sprite in self.obstacle_sprites:
-				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.y > 0: # moving down
-						self.hitbox.bottom = sprite.hitbox.top
-					if self.direction.y < 0: # moving up
-						self.hitbox.top = sprite.hitbox.bottom
 
 	def animate(self):
 		animation = self.animations[self.status]
